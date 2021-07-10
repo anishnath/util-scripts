b8e2f68ebe218284e05cdcb2d1e26c4d98c5c64e
Anish Nath <anish2good@yahoo.co.in>
2021-07-10 20:49:25 +0530

    Update and rename delete-line.py to print-or-delete-line.py

--- print-or-delete-line.py
+++ print-or-delete-line.py
@@ -1,16 +1,17 @@
 import sys
 import os
 
-def delete_lines(original_file, line_numbers):
+def print_or_delete_lines(original_file, line_numbers,action):
     is_skipped = False
-    print(type(line_numbers))
     counter = 0
-    print(type(counter))
     dummy_file = original_file + '.bak'
     with open(original_file, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
         for line in read_obj:
             if counter not in line_numbers:
-                write_obj.write(line)
+                if action == "print":
+                    print(line)
+                else:
+                    write_obj.write(line)
             else:
                 is_skipped = True
             counter += 1
@@ -25,5 +26,8 @@ def delete_lines(original_file, line_numbers):
 user_input = input("Enter the path of your file: ")
 assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
 linerange=[0,1,2]
-delete_lines(user_input,linerange)
+print_or_delete_lines(user_input,linerange,"print")
+linerange=[1,2]
+print_or_delete_lines(user_input,linerange,"delete")
+print_or_delete_lines(user_input,linerange,"print")
 
